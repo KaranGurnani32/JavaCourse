0111fed734367ceb78bf316c6d1e00b9a10a6b06
KaranGurnani32 <karang393@gmail.com>
2022-01-17 19:41:28 +0530

    String Methods

---
+++ JavaCodes/src/basicCodes/StringMethods.java
@@ -0,0 +1,23 @@
+package basicCodes;
+
+import java.util.Locale;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String name = "Karan";
+        System.out.println(name);
+//        name = String.valueOf(name.length());
+//        System.out.println(name);
+        name = name.toLowerCase();
+        System.out.println(name);
+        name = name.toUpperCase();
+        System.out.println(name);
+
+        String nonTrimmedString = "   Karan    ";
+        System.out.println(nonTrimmedString);
+        System.out.println(nonTrimmedString.trim());
+
+        String subString = name.substring(1, 5);
+        System.out.println(subString);
+    }
+}
